4639d13e444d31b6f4764e45e52cdb4132e32be5
Danielid Ramirez <danielid132@gmail.com>
2021-04-27 20:07:40 -0400

    Creando pagina three

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #get 'pages/one'
   get 'pages/two'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/three'
   root 'pages#one'
 end
